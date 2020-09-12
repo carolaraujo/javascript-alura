c96a19d9b543d5a97a2490abaa93195bed5c4f2c
Caroline Araujo <ca.araujosouza@gmail.com>
2020-09-11 23:31:49 -0300

    exclusao

--- 2-trabalhandoComVariaveis.js
+++
@@ -1,8 +0,0 @@
-// console.log("Trabalhando com variáveis");
-// JS é case sensitive
-
-const idade = 29;
-console.log(idade);
-
-// não fazer
-// mes = "Janeiro";
--- javascript-primeiros-passos-com-a-linguagem/2-trabalhandoComVariaveis.js
+++ javascript-primeiros-passos-com-a-linguagem/2-trabalhandoComVariaveis.js
@@ -1,2 +1,8 @@
-console.log("Trabalhando com variáveis");
+// console.log("Trabalhando com variáveis");
 // JS é case sensitive
+
+const idade = 29;
+console.log(idade);
+
+// não fazer
+// mes = "Janeiro";
